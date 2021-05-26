974e5491366739937ea63c54417e1d5ae0d31b6d
ketanb56up <ketanb56up@gmail.com>
2021-05-26 17:19:14 +0530

    Created stock model

--- stock_management_app/models.py
+++ stock_management_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    """
+    Created Stock model with fields stock, close_price and date
+    """
+
+    stock = models.CharField(max_length=50, verbose_name = "Stock")
+    close_price = models.FloatField(verbose_name = "Close Price")
+    date = models.CharField(max_length=50, verbose_name = "Date")
+
+    def __str__(self):
+        return self.stock

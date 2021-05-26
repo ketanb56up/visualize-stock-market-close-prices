54c5a92825f05cda4914a066afb7b675781f9707
ketanb56up <ketanb56up@gmail.com>
2021-05-26 18:28:11 +0530

    Created backend test case

--- stock_management_app/tests.py
+++ stock_management_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Stock
 
-# Create your tests here.
+
+class StockManagementTestCase(TestCase):
+
+    def setUp(self):
+
+        Stock.objects.create(
+        	stock='NVMZ',
+        	close_price='30.0067',
+        	date='2021-05-17'
+        )
+
+    def test_verifiy_stock_data(self):
+        
+        filter_stock = Stock.objects.filter(stock='NVMZ').values()
+
+        self.assertIn('NVMZ', filter_stock.first()['stock'])

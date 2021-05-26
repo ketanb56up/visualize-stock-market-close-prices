99f748d08fc60e3e3f952ed54637741a35b6daa4
ketanb56up <ketanb56up@gmail.com>
2021-05-26 17:25:11 +0530

    Created stock admin to display on admin interface

--- stock_management_app/admin.py
+++ stock_management_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Stock
 
-# Register your models here.
+
+class StockAdmin(admin.ModelAdmin):
+    # Display stocks fields on the admin interface
+
+    list_display = ('stock', 'close_price', 'date')
+
+admin.site.register(Stock, StockAdmin)
